9d794f5684f35079d45c4a1a579a954a03913f73
Mel/ <tchoikuemel06@gmail.com>
2024-09-05 19:36:12 +0100

    feat(ui):maping product

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
 'use client';
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardFooter, CardTitle } from '@/components/ui/card';
-import { ArrowRight, Eye, Heart, Repeat, Star } from 'lucide-react';
+import { products } from '@/db';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import {
+	ArrowRight,
+	Eye,
+	Heart,
+	Repeat,
+	ShoppingCart,
+	Star,
+} from 'lucide-react';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { motion } from 'framer-motion';
@@ -16,6 +31,7 @@ import 'swiper/css/effect-fade';
 import { Pagination, Autoplay, EffectFade } from 'swiper/modules';
 
 const Home = () => {
+	const partOfProduts = products.slice(0, 6);
 	return (
 		<>
 			<section className='w-full h-[calc(100dvh-10.9rem)] flex items-center justify-end text-gray-500 bg-white overflow-hidden relative dark:bg-black dark:text-gray-100'>
@@ -240,7 +256,7 @@ const Home = () => {
 				</div>
 			</section>
 			<section className='container py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-				<Card className='dark:bg-slate-900 rounded-none py-4 cursor-pointer bg-gray-50 hover:dark:bg-slate-800 transition-all duration-300 ease-in-out shadow-sm dark:shadow-none hover:scale-105 group'>
+				<Card className='dark:bg-slate-900 rounded-none py-4 cursor-pointer bg-white/40 hover:dark:bg-slate-800 transition-all duration-300 ease-in-out shadow-sm dark:shadow-none hover:scale-105 group'>
 					<CardContent className='flex  mb-0 pb-0  gap-8'>
 						<div className='w-5/12 h-full '>
 							<Image
@@ -462,9 +478,117 @@ const Home = () => {
 						</Button>
 					</CardFooter>
 				</Card>
-				<div className='flex container'>
-					<div></div>
-					<div></div>
+			</section>
+			<section className=' container w-full gap-5 grid grid-cols-3 overflow-hidden'>
+				<div className='col-span-1'>
+					<Card className='cursor-pointer rounded'>
+						<CardHeader>
+							<CardTitle className='font-light'>Offer special</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<Image
+								src='/consal.webp'
+								alt='consal'
+								loading='lazy'
+								width={400}
+								height={700}
+								className='w-full h-full object-contain'
+								title='consal'
+							/>
+							<h3 className='text-center scroll-m-20 text-sm font-semibold tracking-tight text-blue-600 '>
+								Game Console Controller + USB 3.0 Cable
+							</h3>
+							<p className='scroll-m-20 text-3xl font-normal text-slate-500 tracking-tight first:mt-0 text-center mt-10 mb-5 dark:text-slate-100'>
+								$99.0
+							</p>
+						</CardContent>
+						<CardFooter className='flex-col gap-3'>
+							<div className='flex items-center justify-between w-full'>
+								<p className='text-slate-500 text-xs dark:text-slate-100'>
+									Already Sold: <span className='font-bold '>81</span>
+								</p>
+								<p className='text-slate-500 text-xs dark:text-slate-100'>
+									Available: <span className='font-bold'>19</span>
+								</p>
+							</div>
+							<div className='relative w-full rounded-full bg-gray-100 shadow-inner overflow-hidden h-[10px] dark:bg-slate-800'>
+								<div className='absolute top-0 left-0 w-9/12 bg-[#EF7C1A] h-full rounded-full'></div>
+							</div>
+						</CardFooter>
+					</Card>
+				</div>
+				<div className='col-span-2'>
+					<div
+						className='w-full flex items-center justify-center  border-b border-solid border-gray-200 dark:border-white mb-5 gap-10 font-light text-slate-500
+					dark:text-slate-100
+					'
+					>
+						<button
+							aria-label='tab-bar '
+							className='pb-2 relative flex items-center justify-center before:w-full before:absolute before:-bottom-[0.3px] before:left-0 before:h-[1.5px] before:rounded before:bg-[#EF7C1A] after:absolute after:w-[12px] after:h-1 after:rounded-full after:-bottom-[2.8px] after:bg-[#EF7C1A] after:mx-auto after:left-[50%] after:-translate-x-[50%] font-bold tracking-wide'
+						>
+							Featured
+						</button>
+						<button
+							aria-label='tab-bar'
+							className=' pb-2 relative flex items-center justify-center tracking-wide'
+						>
+							On Sale
+						</button>
+						<button
+							aria-label='tab-bar'
+							className=' pb-2 relative flex items-center justify-center tracking-wide'
+						>
+							Top Rated
+						</button>
+					</div>
+					<div className='w-full grid grid-cols-3 gap-2'>
+						{partOfProduts.map((product) => {
+							return (
+								<article
+									key={product.id}
+									className='h-full w-full overflow-hidden cursor-pointer'
+								>
+									<Card className='rounded pb-1 w-full h-full overflow-hidden flex flex-col justify-between'>
+										<CardHeader className='px-3'>
+											<CardDescription className='flex items-center gap-2'>
+												{product.category?.name.map((categories, index) => {
+													return (
+														<span className='text-xs mb-0.5 ' key={index}>
+															{categories}
+														</span>
+													);
+												})}
+											</CardDescription>
+											<CardTitle className='text-sm font-semibold text-blue-600  mb-0'>
+												{product.name}
+											</CardTitle>
+										</CardHeader>
+										<CardContent className='h-[220px] px-3 mt-0 '>
+											<Image
+												src={product.image}
+												alt={product.name}
+												width={300}
+												height={300}
+												className='w-full h-full object-cover'
+												loading='lazy'
+											/>
+										</CardContent>
+										<CardFooter className='px-3  justify-between  items-center'>
+											<span className='text-xl'>${product.price}</span>
+											<Button
+												aria-label='add to cart'
+												size={'icon'}
+												className='rounded-full'
+											>
+												<ShoppingCart className='w-6 h-6' strokeWidth={1.5} />
+											</Button>
+										</CardFooter>
+									</Card>
+								</article>
+							);
+						})}
+					</div>
 				</div>
 			</section>
 		</>
