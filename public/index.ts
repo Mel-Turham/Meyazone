3b4e428e499ecf4627d933914ab009f07e963ee7
Mel/ <tchoikuemel06@gmail.com>
2024-09-05 19:28:56 +0100

    images for projects added

---
+++ public/index.ts
@@ -0,0 +1,88 @@
+import ipadGreen from './ipadgreen.webp';
+import Camers360 from './360-camers.webp';
+import AirPord from './AirPord.webp';
+import appipad from './appipad.webp';
+import applap from './applap.png';
+import applecare from './applecare.webp';
+import appTablet from './apptablet.webp';
+import asuaslap from './asuaslap.png';
+import cam4k from './cam4k.webp';
+import camera2 from './camera2.webp';
+import cameras_resized from './cameras-resized.webp';
+import consal_300 from './consal-300.webp';
+import consal from './consal.webp';
+import destop from './desktop.webp';
+import footerWidjet from './footer-widget.webp';
+import game1 from './game1.webp';
+import gamecabin from './gamecabin.webp';
+import head from './head.webp';
+import head1 from './heade1.png';
+import headphonecase from './headphonecase.png';
+import iapdlap from './iapdlap.webp';
+import Lgphone from './lgphone.webp';
+import macPro from './macpro.webp';
+import occlod from './occlod.webp';
+import pendirve from './pendirve.webp';
+import powerbanck from './powerbank.png';
+import redmi from './redmi.webp';
+import redPhone from './redPhone.webp';
+import sam1 from './sam1.webp';
+import sam2 from './sam2.webp';
+import segegate from './segagate.webp';
+import Smatphone from './Smartphones.webp';
+import SmartphonResized from './smartwatches-resized.webp';
+import Ultrabooks from './Ultrabooks.png';
+import VideoCamera from './videocamera.webp';
+import Watch from './watch.webp';
+import Whirelesscar from './whirelesscar.webp';
+import WideTv from './widetv.webp';
+import WirelessSound from './WirelessSound.webp';
+import uniheadphone from './uniheadphone.webp';
+import GoldePhone from './GoldPhone.webp'
+import usbheadPhone from './usbheadphone.webp'
+import printer from './printer.webp'
+
+export {
+	usbheadPhone,
+	GoldePhone,
+	printer,
+	uniheadphone,
+	WirelessSound,
+	head,
+	headphonecase,
+	head1,
+	iapdlap,
+	Lgphone,
+	occlod,
+	macPro,
+	SmartphonResized,
+	Ultrabooks,
+	VideoCamera,
+	Smatphone,
+	ipadGreen,
+	applap,
+	redPhone,
+	redmi,
+	sam1,
+	sam2,
+	segegate,
+	camera2,
+	AirPord,
+	appTablet,
+	appipad,
+	applecare,
+	asuaslap,
+	Watch,
+	WideTv,
+	Whirelesscar,
+	cam4k,
+	cameras_resized,
+	consal_300,
+	consal,
+	destop,
+	game1,
+	gamecabin,
+	pendirve,
+	powerbanck,
+	Camers360,
+};
