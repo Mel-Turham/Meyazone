2805ae491729812f5e9939083e350a679e98ceca
Mel/ <tchoikuemel06@gmail.com>
2024-09-05 19:34:07 +0100

    create product fake db

---
+++ db/index.ts
@@ -0,0 +1,402 @@
+import {
+	applap,
+	appTablet,
+	cam4k,
+	camera2,
+	consal_300,
+	gamecabin,
+	GoldePhone,
+	head1,
+	headphonecase,
+	Lgphone,
+	macPro,
+	powerbanck,
+	printer,
+	redmi,
+	redPhone,
+	sam2,
+	Ultrabooks,
+	uniheadphone,
+	Watch,
+	Whirelesscar,
+	WideTv,
+	WirelessSound,
+} from '@/public';
+import { ProductTypes } from '@/types';
+
+const products: ProductTypes[] = [
+	{
+		id: 1,
+		name: 'Wireless Audio System Multiroom 360',
+		image: WirelessSound,
+		category: {
+			name: ['Audio Speakers', 'TV & Audio'],
+		},
+		price: 2999,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 2,
+		name: 'Tablet Red EliteBook Revolve 810 G2',
+		image: appTablet,
+		category: {
+			name: ['Laptops', 'Laptops'],
+		},
+		price: 21100,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 3,
+		name: 'White Solo 2 Wireless',
+		image: uniheadphone,
+		category: {
+			name: ['Accessories', 'Headphones'],
+		},
+		price: 248.99,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 4,
+		name: 'Smartphone 6S 32GB LTE',
+		image: GoldePhone,
+		category: {
+			name: ['Smart Phones', ' Tablets'],
+		},
+		price: 300,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 5,
+		name: 'Purple NX Mini F1 aparat SMART NX',
+		image: camera2,
+		category: {
+			name: ['Cameras'],
+		},
+		price: 559,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 6,
+		name: 'Full Color LaserJet Pro M452dn',
+		image: printer,
+		category: {
+			name: ['Printers', 'Ink', 'Printers', '', ''],
+		},
+		price: 1050,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 7,
+		name: 'Aerocool EN52377 Dead Silence Gaming Cube Case',
+		image: gamecabin,
+		category: {
+			name: ['Computer Cases', ' Computer'],
+		},
+		price: 150,
+		decription: {
+			desc: ['1', '', ''],
+		},
+	},
+	{
+		id: 8,
+		name: 'Smartphone 6S 32GB LTE',
+		image: '',
+		category: {
+			name: ['Smart Phones ', ' Tablets'],
+		},
+		price: 1100,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 9,
+		name: 'Powerbank 1130 mAh Blue',
+		image: powerbanck,
+		category: {
+			name: ['Accessories', ' Power Banks'],
+		},
+		price: 200,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 10,
+		name: 'Smartwatch 2.0 LTE Wifi Waterproof',
+		image: Watch,
+		category: {
+			name: ['Gadgets', 'Smartwatches'],
+		},
+		price: 700,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 11,
+		name: 'Ultrabook UX305CA-FC050T',
+		image: redmi,
+		category: {
+			name: ['Smart Phones &  Tablets,'],
+		},
+		price: 1200,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 12,
+		name: 'Tablet Red EliteBook Revolve 810 G2',
+		image: appTablet,
+		category: {
+			name: ['Laptops, Laptops '],
+		},
+		price: 2100,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 13,
+		name: '',
+		image: headphonecase,
+		category: {
+			name: ['Accessories', 'Headphone'],
+		},
+		price: 159,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 14,
+		name: 'Headphones USB Wires',
+		image: '',
+		category: {
+			name: ['Accessories', 'Headphone'],
+		},
+		price: 50,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 15,
+		name: 'Ultra Wireless S50 Headphones S50 with',
+		image: head1,
+		category: {
+			name: ['Accessories', 'Headphone'],
+		},
+		price: 350,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 16,
+		name: 'Game Console Controller + USB 3.0 Cable',
+		image: consal_300,
+		category: {
+			name: ['Game Consoles', 'Video'],
+		},
+		price: 99,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 17,
+		name: 'Wireless Audio System Multiroom 360',
+		image: '',
+		category: {
+			name: ['Audio Speakers', 'TV & Audio'],
+		},
+		price: 2299,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 18,
+		name: 'Tablet White EliteBook Revolve 810 G2',
+		image: Ultrabooks,
+		category: {
+			name: ['Laptops, Laptops '],
+		},
+		price: 1300,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 19,
+		name: 'Laptop Screener CX70 2QF-621XPL 17.3″ 4210',
+		image: applap,
+		category: {
+			name: ['Laptops', 'Laptops & Computers'],
+		},
+		price: 2399,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 20,
+		name: 'Widescreen 4K SUHD TV',
+		image: WideTv,
+		category: {
+			name: ['Home Entertainment', 'TVs'],
+		},
+		price: 3299,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 21,
+		name: 'Tablet Thin EliteBook Renolve 810 G6',
+		image: redPhone,
+		category: {
+			name: ['Smart Phones', 'Tablets'],
+		},
+		price: 1300,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 22,
+		name: 'Apple MacBook Pro MF84HN/A 13-inch',
+		image: macPro,
+		category: {
+			name: ['Computers', 'Latops'],
+		},
+		price: 1500,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 23,
+		name: 'Camera C430W 4k Waterproof',
+		image: '',
+		category: {
+			name: ['Camers', 'cameras'],
+		},
+		price: 590,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 24,
+		name: 'Notebook Widescreen Z51-70 40K6013UPB',
+		image: sam2,
+		category: {
+			name: ['Smart Phones & Tablets'],
+		},
+		price: 1100,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 25,
+		name: 'Smart Camera 6200U with 500GB',
+		image: cam4k,
+		category: {
+			name: ['Cameras', 'Cameras'],
+		},
+		price: 2999,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 26,
+		name: 'Wireless Charger 2040 White',
+		image: Whirelesscar,
+		category: {
+			name: ['Accessories'],
+		},
+		price: 248,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 27,
+		name: 'Smartphones 6S 32GB LTE',
+		image: Lgphone,
+		category: {
+			name: ['Smart Phones'],
+		},
+		price: 780,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 28,
+		name: '',
+		image: '',
+		category: {
+			name: ['', '', '', ''],
+		},
+		price: 300,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 29,
+		name: '',
+		image: '',
+		category: {
+			name: ['', '', '', ''],
+		},
+		price: 300,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 30,
+		name: '',
+		image: '',
+		category: {
+			name: ['', '', '', ''],
+		},
+		price: 300,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+	{
+		id: 31,
+		name: '',
+		image: '',
+		category: {
+			name: ['', '', '', ''],
+		},
+		price: 300,
+		decription: {
+			desc: ['', '', ''],
+		},
+	},
+];
+
+export { products };
