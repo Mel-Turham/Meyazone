696cfa8401f68c4aae7ec7293e86e77811de66de
Mel/ <tchoikuemel06@gmail.com>
2024-09-22 20:58:44 +0100

    style:loading.tsx
    add style for loading component

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+	return (
+		<div className='fixed top-0 left-0 w-full h-screen bg-black flex items-center justify-center text-white z-20 text-6xl font-bold'>
+			loading...
+		</div>
+	);
+};
+
+export default Loading;
