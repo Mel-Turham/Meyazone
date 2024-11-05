32b407db8c252b60a3929e6f6bd3b2a330f0e484
Mel-Turham <tchoikuemel06@gmail.com>
2024-11-05 18:19:07 +0100

    feat(ui):add users management page

--- app/dashboards/admin/admin-sidebar.tsx
+++ app/dashboards/admin/admin-sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useToggleAdminSideBar } from '@/store/useToggleAdminSideBar';
-import { LayoutDashboard } from 'lucide-react';
+import { LayoutDashboard, UserCheck } from 'lucide-react';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
@@ -34,7 +34,7 @@ const AdminSideBar = () => {
           className='transition-all ease-in-out duration-300 w-[45px]'
         />
         {toggleSideBar ? null : (
-          <h1 className='text-xl font-black'>MEYAZONE</h1>
+          <h1 className='text-xl font-extrabold text-primary'>MEYAZONE</h1>
         )}
       </div>
       <div className='min-h-full relative'>
@@ -66,6 +66,37 @@ const AdminSideBar = () => {
                   className='absolute dark:bg-slate-900 p-2 font-medium bg-gray-200 z-20 -right-[115px] rounded'
                 >
                   Dashbord
+                </motion.span>
+              )}
+            </AnimatePresence>
+          </div>
+          <div
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className={`w-full flex items-center dark:hover:bg-slate-900/70 hover:bg-gray-200 gap-2 px-2 py-1.5 cursor-pointer rounded ${
+              toggleSideBar && 'justify-center'
+            } ${
+              pathname === '/dashboards/admin'
+                ? 'bg-gray-200  dark:bg-slate-900/70'
+                : ''
+            }`}
+          >
+            <UserCheck className='w-6 h-6' strokeWidth={1.5} />
+            {toggleSideBar ? null : (
+              <span className='text-base font-medium'>Users</span>
+            )}
+            <AnimatePresence>
+              {toggleSideBar && isHovered && (
+                <motion.span
+                  initial={{ opacity: 0, scale: 0.85 }}
+                  animate={{
+                    opacity: isHovered ? 1 : 0,
+                    scale: isHovered ? 1 : 0.85,
+                  }}
+                  exit={{ opacity: 0, scale: 0.85 }}
+                  className='absolute dark:bg-slate-900 p-2 font-medium bg-gray-200 z-20 -right-[115px] rounded'
+                >
+                  Users
                 </motion.span>
               )}
             </AnimatePresence>
