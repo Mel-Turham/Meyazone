d124a02bbb8a3a8f13703ca4963c2cb99ac4d9bb
Mel-Turham <tchoikuemel06@gmail.com>
2024-11-24 00:09:33 +0100

    update formatprice function

--- utils/formatPrice.ts
+++ utils/formatPrice.ts
@@ -1,18 +1,18 @@
-function formatPrice(price: number, currency: 'USD' | 'XOF'): string {
-	const formatter = new Intl.NumberFormat(
-		currency === 'USD' ? 'en-US' : 'fr-FR',
-		{
-			style: 'currency',
-			currency: currency,
-			minimumFractionDigits: 2,
-			maximumFractionDigits: 3,
-		},
-	);
+function formatPrice(price: number, currency: 'USD' | 'XOF') {
+  const formatter = new Intl.NumberFormat(
+    currency === 'USD' ? 'en-US' : 'fr-FR',
+    {
+      style: 'currency',
+      currency: currency,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 3,
+    }
+  );
 
-	const exchangeRate = 655.957; // Fixed exchange rate between USD and XOF
-	const currentPrice = currency === 'USD' ? price : price * exchangeRate;
+  const exchangeRate = 655.957; // Fixed exchange rate between USD and XOF
+  const currentPrice = currency === 'USD' ? price : price * exchangeRate;
 
-	return formatter.format(currentPrice);
+  return formatter.format(currentPrice);
 }
 
 export default formatPrice;
